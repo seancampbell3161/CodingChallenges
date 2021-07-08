b4feb9d8a2bb43dd36a8998cd8073ded52c67330
Sean Campbell <sean.campbell3161@gmail.com>
2021-07-08 10:01:38 -0500

    create function that returns true if employed is true and vacation is false, otherwise return false

---
+++ JavaScript/CodeWars/SetAlarm.js
@@ -0,0 +1,10 @@
+// The function should return true if you are employed and not on vacation.
+//It should return false otherwise. 
+
+function setAlarm(employed, vacation){
+  
+    if(employed === true && vacation === false) return true;
+    
+    return false;
+  
+  }
