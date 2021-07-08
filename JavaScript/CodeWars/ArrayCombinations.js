9b6beaa63de41e2c7bc7a789571b08add8309594
Sean Campbell <sean.campbell3161@gmail.com>
2021-07-08 10:44:44 -0500

    find the number of unique arrays that can be formed by picking 1 element from each subarray

---
+++ JavaScript/CodeWars/ArrayCombinations.js
@@ -0,0 +1,32 @@
+/*
+
+In this Kata, you will be given an array of arrays and your task will be to return the number of unique arrays 
+that can be formed by picking exactly one element from each subarray.
+
+For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
+
+Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra outcomes are just duplicates.
+
+
+*/
+
+function solve(arr) {
+  
+    for(i = 0; i < arr.length; i++) {
+      arr[i].sort();
+      for(j = 0; j < arr[i].length; j++) {
+        if(arr[i][j] === arr[i][j+1]) {
+          arr[i].splice(j, 1);
+        }
+      }
+    }
+    
+    result = arr[0].length;
+    
+    for(i = 1; i < arr.length; i++) {
+      result *= arr[i].length;
+    }
+    
+    return result;
+    
+  };
