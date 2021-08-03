0b2c74105f8f4fa31b38e0a0fe0e8aa48c135219
Sean Campbell <sean.campbell3161@gmail.com>
2021-08-02 22:37:48 -0500

    find the maximum value of every combination of mathematical expressions

--- JavaScript/CodeWars/MaximumExpression.js
+++ JavaScript/CodeWars/MaximumExpression.js
@@ -6,22 +6,12 @@ function expressionMatter(a, b, c) {
   
     let values = [];
     
-    let multiplyAll = a * b * c;
-    let addAll = a + b + c;
-    let multiplyAddPar = a * (b + c);
-    let parAddMultiply = (a + b) * c;
-    let addMultiply = a + b * c;
-    let multiplyAdd = a * b + c;
-  
-    
-    values.push(multiplyAll);
-    values.push(multiplyAddPar);
-    values.push(addMultiply);
-    values.push(parAddMultiply);
-    values.push(addAll);
-    values.push(multiplyAdd);
-    
-    values.sort();
+    values.push(a * b * c);
+    values.push(a + b + c);
+    values.push(a * (b + c));
+    values.push((a + b) * c);
+    values.push(a + b * c);
+    values.push(a * b + c);
     
     return Math.max(...values);
   }
