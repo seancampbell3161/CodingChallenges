7e3da2730d5d483704325af2ee7e5501c46bd4b8
Sean Campbell <sean.campbell3161@gmail.com>
2021-08-11 15:48:12 -0500

    create function that will add all of the arguments

---
+++ JavaScript/CodeWars/MessiGoals.js
@@ -0,0 +1,7 @@
+/*
+Complete the function to return his total number of goals in all three leagues.
+*/
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
