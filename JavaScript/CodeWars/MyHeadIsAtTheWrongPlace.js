ddd1121041c9b9d71449720f124f84d452ee3008
Sean Campbell <sean.campbell3161@gmail.com>
2021-08-12 10:08:15 -0500

    rearrange array to show the correct order

---
+++ JavaScript/CodeWars/MyHeadIsAtTheWrongPlace.js
@@ -0,0 +1,8 @@
+/*
+You will be given an array which will have three values (tail, body, head). It is your job 
+to re-arrange the array so that the animal is the right way round (head, body, tail).
+*/
+
+function fixTheMeerkat(arr) {
+    return arr.reverse();
+   }
